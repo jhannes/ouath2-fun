c4a3b000d6488d4af228c98472c0f95bc1e42fb3
Johannes Brodwall <johannes@brandmaster.com>
2014-09-06 07:09:20 +0200

    Updated to changes in server

--- oauth2-fun-service/src/main/java/com/johannesbrodwall/oauth2fun/service/ServiceApplicationServer.java
+++ oauth2-fun-service/src/main/java/com/johannesbrodwall/oauth2fun/service/ServiceApplicationServer.java
@@ -1,5 +1,7 @@
 package com.johannesbrodwall.oauth2fun.service;
 
+import org.slf4j.bridge.SLF4JBridgeHandler;
+
 import com.johannesbrodwall.oauth2fun.lib.ApplicationServer;
 
 public class ServiceApplicationServer extends ApplicationServer {
@@ -9,6 +11,8 @@ public class ServiceApplicationServer extends ApplicationServer {
     }
 
     public static void main(String[] args) throws Exception {
+        SLF4JBridgeHandler.removeHandlersForRootLogger();
+        SLF4JBridgeHandler.install();
         ServiceApplicationServer server = new ServiceApplicationServer(12080);
         server.addHandler(server.shutdownHandler());
         server.addHandler(server.createWebAppContext("/"));
